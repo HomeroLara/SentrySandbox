f296519826e506afeb8d15e900ecb850841c8980
Homero Lara <homero.lara@gmail.com>
2026-09-01 09:20:01 -0500

    Add click counter functionality and update UI elements

--- PollySandbox/MainPage.xaml.cs
+++ PollySandbox/MainPage.xaml.cs
@@ -13,12 +13,22 @@ public partial class MainPage : ContentPage
     {
         try
         {
+            CounterLabel.Text = "Fetching Theaters...";
             var cnkTestTheatreService = ServiceHelper.GetService<ICnkTestTheatreService>();
             var theatres = await cnkTestTheatreService.SearchByGeoCoordinates(32.909946f, -96.87241f, 50f, 5);
         }
         catch (Exception exception)
         {
             Console.WriteLine(exception);
+            CounterLabel.Text = "Error: " + exception.Message;
         }
+    }
+    
+    static int counter = 0;
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        counter++;
+        CounterLabel.Text = $"You clicked {counter} times";
     }
 }
